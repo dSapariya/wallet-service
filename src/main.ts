bd9a8d9db49809b201d5f26ef3f6cc7ff22ea1aa
dSapariya <dixasapariya786@gmail.com>
2025-08-04 21:45:08 +0530

    enable cors for the frontend

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: ['http://localhost:3000', 'https://wallet-system-theta.vercel.app'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true, // if you're using cookies or auth headers
+  });
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
